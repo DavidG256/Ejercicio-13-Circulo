ebbbb6b45d477c4dca5ca461444b22c92c0f4d96
DavidG256 <davgarant@alu.edu.gva.es>
2025-01-21 09:54:47 +0100

    Update mainEJ13.java

--- mainEJ13.java
+++ mainEJ13.java
@@ -2,12 +2,12 @@ public class mainEJ13 {
     public static void main(String[] args) {
 
         Circulo circulo = new Circulo(7);
-        System.out.println("Radio del círculo: " + circulo.getRadio());
-        System.out.println("Área del círculo: " + circulo.getArea());
+        System.out.println("Radio círculo: " + circulo.getRadio());
+        System.out.println("Área círculo: " + circulo.getArea());
 
 
         Cilindro cilindro = new Cilindro(3, 6);
-        System.out.println("Volumen del cilindro: " + cilindro.getVolume());
+        System.out.println("Volumen cilindro: " + cilindro.getVolume());
 
 
     }
